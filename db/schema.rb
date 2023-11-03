c310bbf08c576b9949a696269f259d77b76e4c64
naveen2112 <prnaveen2112@gmail.com>
2023-11-03 11:31:14 +0530

    1st comit

--- db/schema.rb
+++ db/schema.rb
@@ -69,33 +69,6 @@ ActiveRecord::Schema.define(version: 20170923025443) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "names", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer "sign_in_count", default: 0, null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string "current_sign_in_ip"
-    t.string "last_sign_in_ip"
-    t.date "dob"
-    t.string "gender"
-    t.string "image"
-    t.string "phone"
-    t.string "address"
-    t.string "lat"
-    t.string "long"
-    t.string "last_login_ip"
-    t.integer "parent_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_names_on_email", unique: true
-    t.index ["parent_id"], name: "index_names_on_parent_id"
-    t.index ["reset_password_token"], name: "index_names_on_reset_password_token", unique: true
-  end
-
   create_table "parents", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
